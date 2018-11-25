54967af2458df224cb1044550f19b14ca2a2a5de
DenisNolimit <44726988+DenisNolimit@users.noreply.github.com>
2018-11-26 02:21:08 +0300

    Add files via upload

---
+++ MyMephi/AuthorisationPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyMephi
+{
+
+	public partial class AuthorisationPage : ContentPage
+	{
+		public AuthorisationPage ()
+		{
+            InitializeComponent ();
+
+            Title = "Авторизация";
+		}
+
+        private async void ToBarPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BarPage());
+        }
+	}
+}
